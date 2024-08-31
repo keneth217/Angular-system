b8c408604735a19017cd571a8b872f77e595b70a
keneth217 <kipyegonkeneth03@gmail.com>
2024-08-31 14:58:57 +0300

    products details

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,28 +1,31 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { AddProductComponent } from '../add-product/add-product.component';
+
 import { AngularFireDatabase } from '@angular/fire/compat/database';
 import { Observable } from 'rxjs';
+import { ProductEditComponent } from '../services/product-edit/product-edit.component';
+import { ProductViewComponent } from '../services/product-view/product-view.component';
 
 @Component({
   selector: 'app-product-list',
   standalone: true,
-  imports: [CommonModule, AddProductComponent],
+  imports: [CommonModule, AddProductComponent, ProductEditComponent, ProductViewComponent],
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent implements OnInit {
   products: Observable<any[]> | undefined;
   showAddProductForm = false;
+  showViewProduct = false;
+  showEditProduct = false;
   isLoading: boolean = true;
+  selectedProduct: any;
 
   constructor(private db: AngularFireDatabase) {}
 
   ngOnInit() {
-    // Set loading to true initially
     this.isLoading = true;
-
-    // Fetch products and manage loading state
     this.products = this.db.list('products').valueChanges();
     this.products.subscribe({
       next: () => this.isLoading = false,
@@ -32,5 +35,24 @@ export class ProductListComponent implements OnInit {
 
   toggleAddProductForm() {
     this.showAddProductForm = !this.showAddProductForm;
+  }
+
+  viewProduct(product: any) {
+    this.selectedProduct = product;
+    this.showViewProduct = true;
+  }
+
+  editProduct(product: any) {
+    this.selectedProduct = product;
+    this.showEditProduct = true;
+  }
+
+  deleteProduct(productId: string) {
+    this.db.list('products').remove(productId);
+  }
+
+  updateProduct(product: any) {
+    this.db.list('products').update(product.id, product);
+    this.showEditProduct = false;
   }
 }
