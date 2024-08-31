4f419c3b124ce15e955b1cccec0dc9cfa3dddb78
keneth217 <kipyegonkeneth03@gmail.com>
2024-08-31 13:49:52 +0300

    add components for pos

---
+++ src/app/add-creditor/add-creditor.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-creditor',
+  standalone: true,
+  imports: [],
+  templateUrl: './add-creditor.component.html',
+  styleUrl: './add-creditor.component.css'
+})
+export class AddCreditorComponent {
+
+}
