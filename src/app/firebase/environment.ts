842bd270613ed75b67814ee2cead368a8dcc555f
keneth217 <kipyegonkeneth03@gmail.com>
2024-08-31 13:52:04 +0300

    add components for pos

---
+++ src/app/firebase/environment.ts
@@ -0,0 +1,10 @@
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB7pYFK-MoS2bGFzvqITRycTxOwQXt1BPE",
+  authDomain: "possystem-711b5.firebaseapp.com",
+  databaseURL: "https://possystem-711b5-default-rtdb.firebaseio.com",
+  projectId: "possystem-711b5",
+  storageBucket: "possystem-711b5.appspot.com",
+  messagingSenderId: "745170978236",
+  appId: "1:745170978236:web:7c0929a6c719cd4747d7b9"
+};
