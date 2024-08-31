6f420c6bbc8d0905394aadf52816ec0f158e02a1
keneth217 <kipyegonkeneth03@gmail.com>
2024-08-31 13:50:46 +0300

    add components for pos

---
+++ src/app/card-component/card-component.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-card-component',
+  standalone: true,
+  imports: [],
+  templateUrl: './card-component.component.html',
+  styleUrl: './card-component.component.css'
+})
+export class CardComponentComponent {
+  @Input()
+  title!: string;
+
+  @Input()
+  value!: string;
+}
