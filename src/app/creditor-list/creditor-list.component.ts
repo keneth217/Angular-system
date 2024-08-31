cf2f689a0782aa161f92664486eaed274eebf96a
keneth217 <kipyegonkeneth03@gmail.com>
2024-08-31 13:51:20 +0300

    add components for pos

---
+++ src/app/creditor-list/creditor-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-creditor-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './creditor-list.component.html',
+  styleUrl: './creditor-list.component.css'
+})
+export class CreditorListComponent {
+
+}
