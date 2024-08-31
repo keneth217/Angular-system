bf95e5a2fbf7f56149bd819b67d52504baf7b787
keneth217 <kipyegonkeneth03@gmail.com>
2024-08-31 13:52:24 +0300

    add components for pos

---
+++ src/app/models/products.model.ts
@@ -0,0 +1,13 @@
+export interface Product {
+  batch: string;
+  name: string;
+  description: string;
+  costPrice: number;
+  sellingPrice: number;
+  quantity: number;
+  supplier: string;
+  category: string;
+  warehouse: string;
+  expiryDate: string;
+  imageUrl: string;
+}
