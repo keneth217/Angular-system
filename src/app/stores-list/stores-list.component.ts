95193cfd848d4b985119b9ab1d45ab56dae825ac
keneth217 <kipyegonkeneth03@gmail.com>
2024-08-31 14:34:33 +0300

    stores in pos

--- src/app/stores-list/stores-list.component.ts
+++ src/app/stores-list/stores-list.component.ts
@@ -1,12 +1,38 @@
 import { Component } from '@angular/core';
+import { AngularFireDatabase } from '@angular/fire/compat/database';
+import { Observable } from 'rxjs';
+import { AddStoreComponent } from "../add-store/add-store.component";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-stores-list',
   standalone: true,
-  imports: [],
+  imports: [AddStoreComponent, CommonModule],
   templateUrl: './stores-list.component.html',
-  styleUrl: './stores-list.component.css'
+  styleUrls: ['./stores-list.component.css']
 })
 export class StoresListComponent {
+  stores: Observable<any[]> | undefined;
+  showAddStoreForm = false;
+  loading: boolean = true;
 
+  constructor(private db: AngularFireDatabase) {}
+
+  ngOnInit() {
+    this.fetchStores();
+  }
+
+  fetchStores() {
+    this.loading = true;
+    this.stores = this.db.list('stores').valueChanges();
+    this.stores.subscribe(() => {
+      this.loading = false;
+    }, () => {
+      this.loading = false; // Ensure loading is stopped even if there's an error
+    });
+  }
+
+  toggleAddStoreForm() {
+    this.showAddStoreForm = !this.showAddStoreForm;
+  }
 }
