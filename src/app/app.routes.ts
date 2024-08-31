210ffa956de5211edc56fe172c0a9b9859ded2b5
keneth217 <kipyegonkeneth03@gmail.com>
2024-08-31 13:53:26 +0300

    add components for pos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { DashboardComponentComponent } from './dashboard-component/dashboard-component.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { CategoryListComponent } from './category-list/category-list.component';
+import { UsersListComponent } from './users-list/users-list.component';
+import { SupplierListComponent } from './supplier-list/supplier-list.component';
+import { StoresListComponent } from './stores-list/stores-list.component';
+import { CreditorListComponent } from './creditor-list/creditor-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: "", redirectTo: "dash", pathMatch: "full" },
+  {
+      path:"dash", component:DashboardComponentComponent, title:"Admin Dashboard" },
+  { path: "products",component:ProductListComponent },
+  { path:"category",component:CategoryListComponent },
+  { path:"user",component:UsersListComponent },
+  { path:"store",component:StoresListComponent },
+  { path:"supplier",component:SupplierListComponent },
+  { path:"creditor",component:CreditorListComponent },
+];
