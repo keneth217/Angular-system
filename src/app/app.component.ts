9937a7ab512b232b4d5e9caa1f0bd69d8ff57799
keneth217 <kipyegonkeneth03@gmail.com>
2024-08-31 13:53:45 +0300

    add components for pos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,28 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterModule, RouterOutlet } from '@angular/router';
+import { DashboardComponentComponent } from './dashboard-component/dashboard-component.component';
+import { ProductListComponent } from './product-list/product-list.component';
 
+
+const shadowClasses = 'shadow-lg';
+const bgClasses = 'bg-accent';
+const textClasses = 'text-white';
+const hoverClasses = 'hover:bg-secondary/80 transition';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterLink,RouterModule, CommonModule, DashboardComponentComponent, ProductListComponent,
+    RouterOutlet
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'system';
+  title = 'point-of-sale';
+
+  shadowClasses = shadowClasses;
+  bgClasses = bgClasses;
+  textClasses = textClasses;
+  hoverClasses = hoverClasses;
 }
