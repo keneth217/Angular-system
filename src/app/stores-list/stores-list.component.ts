41f775a5446d9b0184b5a87059870f999cf78346
keneth217 <kipyegonkeneth03@gmail.com>
2024-08-31 13:52:29 +0300

    add components for pos

---
+++ src/app/stores-list/stores-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-stores-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './stores-list.component.html',
+  styleUrl: './stores-list.component.css'
+})
+export class StoresListComponent {
+
+}
