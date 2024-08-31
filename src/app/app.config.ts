632dbb4644f984a3ebb6334e0174ced736f23c35
keneth217 <kipyegonkeneth03@gmail.com>
2024-08-31 13:55:32 +0300

    add tailwind css

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,30 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { AngularFireDatabaseModule } from '@angular/fire/compat/database'
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore'
 
 import { routes } from './app.routes';
 
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB7pYFK-MoS2bGFzvqITRycTxOwQXt1BPE",
+  authDomain: "possystem-711b5.firebaseapp.com",
+  databaseURL: "https://possystem-711b5-default-rtdb.firebaseio.com",
+  projectId: "possystem-711b5",
+  storageBucket: "possystem-711b5.appspot.com",
+  messagingSenderId: "745170978236",
+  appId: "1:745170978236:web:7c0929a6c719cd4747d7b9"
+};
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }),
+  provideRouter(routes),
+  importProvidersFrom([AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireAuthModule,
+    AngularFireDatabaseModule,
+    AngularFirestoreModule
+  ])]
 };
